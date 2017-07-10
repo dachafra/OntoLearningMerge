d93b4cad78e8aef7b761b8998a3c34a1a68e62a3
dachafra <dachafra@gmail.com>
2017-07-10 13:51:06 +0200

    Improvements for merging globally

--- src/main/java/com/oeg/Merge.java
+++ src/main/java/com/oeg/Merge.java
@@ -1,26 +1,34 @@
 package com.oeg;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by dchaves on 7/7/17.
  */
-public class MergeRelations {
+public class Merge {
+    private static final Double threshold=0.95;
     private ArrayList<File> files;
-     private HashMap<String,boolean[]> merge;
+    private HashMap<String,boolean[]> merge;
+    private String type;
+
+
+    public Merge(String type){
+        files = new ArrayList<File>();
+        merge = new HashMap<String, boolean[]>();
+        this.type=type;
+    }
 
 
     public void run(){
-        files = new ArrayList<File>();
-        merge = new HashMap<String, boolean[]>();
-        this.readApproaches("/Users/dchaves/Documents/Otros/Approaches");
+        this.readApproaches("<DIR>");
         this.merge();
+        if(type.equals("Terms")){
+            removeSimilarities();
+        }
         this.writeMerge();
     }
 
@@ -32,14 +40,16 @@ public class MergeRelations {
         for(File f1 : listOfFiles){
             files.add(f1);
         }
-
-
     }
 
     private void writeMerge(){
-        String output="Word1,Word2,";
+        String output;
+        if(this.type.equals("Relations"))
+             output="Word1,Word2,";
+        else
+            output="Word1,";
         for(File f : files){
-            output+=f.getName()+",";
+            output+= (new StringTokenizer(f.getName(),".")).nextToken()+",";
         }
         output+="\n";
         ArrayList<String> words= new ArrayList<String>(merge.keySet());
@@ -67,7 +77,6 @@ public class MergeRelations {
 
     private void merge(){
         try {
-
             for(File f : files){
                 List<String> s = FileUtils.readLines(f, "UTF-8");
                 for(String line : s){
@@ -81,6 +90,33 @@ public class MergeRelations {
         }catch (IOException e){
             System.out.println("Error opening a file! "+e.getMessage());
         }
+
+    }
+
+    private void removeSimilarities(){
+        ArrayList<String> terms = new ArrayList<String>(merge.keySet());
+        Collections.sort(terms, new Comparator<String>() {
+            public int compare(String s1, String s2) {
+                return s1.compareToIgnoreCase(s2);
+            }
+        });
+        for(String t: terms){
+            for(String t1: terms){
+                double similarity=StringUtils.getJaroWinklerDistance(t,t1);
+                if(similarity>this.threshold && !t.equals(t1)){
+                    boolean[] results1 = merge.get(t1);
+                    boolean[] results = merge.get(t);
+                    for(int j=0; j<results1.length;j++){
+                        if(results1[j]==true){
+                            results[j]=true;
+                        }
+                    }
+                    merge.remove(t);
+                    merge.put(t,results);
+                }
+            }
+        }
+
 
     }
 
--- src/main/java/com/oeg/MergeOL.java
+++ src/main/java/com/oeg/MergeOL.java
@@ -1,17 +1,11 @@
 package com.oeg;
 
 
-import org.apache.commons.lang3.StringUtils;
-
-import java.io.*;
-import java.util.*;
-
-
 public class MergeOL
 {
 
     public static void main( String[] args ){
-    MergeRelations r = new MergeRelations();
+    Merge r = new Merge("Relations/Terms");
     r.run();
 
 
