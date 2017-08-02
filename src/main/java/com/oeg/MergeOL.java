9c2a0f97cbb10fbfe541c81956f99479a9521c55
dachafra <dachafra@gmail.com>
2017-08-02 16:35:29 +0200

    Improvements for merging globally

--- src/main/java/com/oeg/MergeOL.java
+++ src/main/java/com/oeg/MergeOL.java
@@ -4,10 +4,9 @@ package com.oeg;
 public class MergeOL
 {
 
-    public static void main( String[] args ){
-    Merge r = new Merge("Relations/Terms");
-    r.run();
-
+    public static void main( String[] args ) {
+        Merge r = new Merge("Relations/Terms");
+        r.run();
 
     }
 
